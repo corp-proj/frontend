3f7ecb6d8c55ded7260f809050018407e07135b2
박수연 <sooyeon@MacBookAir.local>
2022-03-08 00:38:47 +0900

    [ADD] initial layout

---
+++ app/src/main/java/com/example/corp_project/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.corp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuInflater
+import androidx.appcompat.app.ActionBar
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
