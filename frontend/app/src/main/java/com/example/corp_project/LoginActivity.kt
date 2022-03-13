912cd40d2bc36668d21c9737f7fe663972d0c6f2
juu924 <juu924>
2022-03-13 23:58:46 +0900

    [ADD] MainPage

---
+++ frontend/app/src/main/java/com/example/corp_project/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.corp_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.corp_project.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_login)
+        //액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this@LoginActivity, "오늘은 어떤 뉴스를 보시겠어요?", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, TagActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ frontend/app/src/main/java/com/example/corp_project/Tag.kt
@@ -0,0 +1,5 @@
+package com.example.corp_project
+
+import java.io.Serializable
+
+data class Tag(var name:String):Serializable
