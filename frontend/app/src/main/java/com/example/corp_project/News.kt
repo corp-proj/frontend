7f4fd0c93c1797591295f5e81d5ab4eae99071c4
juu924 <juu924>
2022-03-13 23:52:03 +0900

    delete folder

--- frontend/app/src/main/java/com/example/corp_project/News.kt
+++
@@ -1,3 +0,0 @@
-package com.example.corp_project
-
-class News (var title:String)
--- frontend/app/src/main/java/com/example/corp_project/Tag.kt
+++
@@ -1,5 +0,0 @@
-package com.example.corp_project
-
-import java.io.Serializable
-
-data class Tag(var name:String):Serializable
