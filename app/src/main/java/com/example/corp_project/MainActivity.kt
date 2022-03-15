08f7974de62bac3914d6c05548315abb6705bd3f
juu924 <juu924>
2022-03-16 00:48:28 +0900

    [modify] 상세 페이지

--- app/src/main/java/com/example/corp_project/MainActivity.kt
+++ app/src/main/java/com/example/corp_project/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.corp_project
 
+import android.R.array
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.corp_project.databinding.ActivityMainBinding
 import com.example.corp_project.fragment.NewsList1
 import com.example.corp_project.fragment.NewsList2
 import com.example.corp_project.fragment.NewsList3
+import org.json.JSONArray
+import org.json.JSONException
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mbinding: ActivityMainBinding
-    var clickcnt = 1;
+    var clickcnt = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,18 +28,19 @@ class MainActivity : AppCompatActivity() {
         val view = mbinding.root
         setContentView(view)
 
-        val tagData = intent.getStringArrayListExtra("tagList")
-        
+        var tagData = intent.getStringArrayListExtra("tagList")
+
+
         //사용자가 선택한 태그를 메인화면에 보여주는 코드
         mbinding.mainTag1.setText(tagData?.get(0))
         mbinding.mainTag2.setText(tagData?.get(1))
-        mbinding.mainTag3.setText(tagData?.get(2))
-        mbinding.mainTag4.setText(tagData?.get(3))
-        mbinding.mainTag5.setText(tagData?.get(4))
-        
+        mbinding.mainTag2.setText(tagData?.get(2))
+        mbinding.mainTag2.setText(tagData?.get(3))
+        mbinding.mainTag2.setText(tagData?.get(4))
 
-        
-       //관심있는 해시태그를 등록했을 떄, 관련 기사 제목들을 보여줄 수 있도록 설계
+
+
+        //관심있는 해시태그를 등록했을 떄, 관련 기사 제목들을 보여줄 수 있도록 설계
         mbinding.mainTag1.setOnClickListener {
             if (clickcnt % 2 != 0) {
                 mbinding.mainTag1.setBackgroundResource(R.drawable.input_box)
@@ -43,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                     R.id.news_list_page,
                     NewsList1()
                 )
-                transaction.commit()
+                transaction.addToBackStack(null).commit()
             } else {
                 mbinding.mainTag1.setBackgroundResource(R.drawable.suggest_word)
                 mbinding.mainTag1.setTextColor(Color.parseColor("#000000"))
@@ -63,7 +71,7 @@ class MainActivity : AppCompatActivity() {
                     R.id.news_list_page,
                     NewsList2()
                 )
-                transaction.commit()
+                transaction.addToBackStack(null).commit()
             } else {
                 mbinding.mainTag2.setBackgroundResource(R.drawable.suggest_word)
                 mbinding.mainTag2.setTextColor(Color.parseColor("#000000"))
@@ -83,7 +91,9 @@ class MainActivity : AppCompatActivity() {
                     R.id.news_list_page,
                     NewsList3()
                 )
-                transaction.commit()
+                transaction.addToBackStack(null).commit()
+
+
             } else {
                 mbinding.mainTag3.setBackgroundResource(R.drawable.suggest_word)
                 mbinding.mainTag3.setTextColor(Color.parseColor("#000000"))
@@ -95,8 +105,8 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    
-    
+
+
     //프래그먼트를 제거하는 함수
     private fun removeFragment() {
         val transaction = supportFragmentManager.beginTransaction()
--- app/src/main/java/com/example/corp_project/NewsAdapter.kt
+++ app/src/main/java/com/example/corp_project/NewsAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.corp_project
 
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.corp_project.databinding.NewsListBinding
 
+
 class NewsAdapter : RecyclerView.Adapter<NewsHolder>() {
     var NewsList = mutableListOf<News>()
+    lateinit var nbinding: NewsListBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
         return NewsHolder(
@@ -28,9 +33,17 @@ class NewsAdapter : RecyclerView.Adapter<NewsHolder>() {
         val news = NewsList.get(position)
         holder.setTitle(news)
 
-        //간격 조절
+        //리스트를 클릭했을 때 뉴스 보기 화면으로 전환
+        holder. nbinding.newsTitle.setOnClickListener(View.OnClickListener { v ->
+            var intent = Intent(v.context, DetailActivity::class.java)
+            intent.putExtra("title", news.toString())
+            v.context.startActivity(intent)
+        })
+
+
+       //간격 조절
         val layoutParams = holder.itemView.layoutParams
-        layoutParams.height = 130
+        layoutParams.height = 150
         holder.itemView.requestLayout()
 
     }
@@ -44,3 +57,4 @@ class NewsHolder(val nbinding: NewsListBinding) : RecyclerView.ViewHolder(nbindi
 
 
 }
+
